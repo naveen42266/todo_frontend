c50c5b8f1283fd450499e49eb4e057f50e727922
Naveen <vnaveenlgp2001@gmail.com>
2024-06-12 15:06:22 +0530

    for deployment

--- src/App.js
+++ src/App.js
@@ -40,8 +40,8 @@ export default function App() {
 	const taskSummary = useRef('');
 
 
-	let API_URL = "http://localhost:8080/"
-	// "todobackend-production-4a5e.up.railway.app"
+	let API_URL = "https://todo-backend-api-cyan.vercel.app/"
+	// "https://todo-backend-api-cyan.vercel.app/"
 	// "https://todo-backend-neon-zeta.vercel.app/"
 	// "http://localhost:8080/"
 
